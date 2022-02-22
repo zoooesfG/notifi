09b70493012beeade0a1a104ee9255b8d40f9bc4
zoooesfG <92531204+zoooesfG@users.noreply.github.com>
2022-02-22 14:42:52 -0500

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+// pages/_app.js
+import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0';
+import { ChakraProvider } from '@chakra-ui/react';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </ChakraProvider>
+  );
+}
