3bfd1e1ff417011a609cd1e38918b2d51f493f20
zoooesfG <zoe.ferguson@globacore.com>
2022-03-02 15:50:43 -0500

    Dave help to get firebase and twilio communicating

--- components/PlayerRow.js
+++ components/PlayerRow.js
@@ -3,35 +3,17 @@
 
 import { Stack, HStack, VStack, Box, StackDivider, Link, Flex, Spacer, Button, StylesProvider } from '@chakra-ui/react'
 import styles from '../styles/Home.module.css'
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, onSnapshot,
-    addDoc, deleteDoc, doc,
-    query, where,
-    orderBy, serverTimestamp,
-    updateDoc } from "firebase/firestore";
+import { useState } from 'react'
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA5qsxUaqCK926zTNiTSFoFHy-BK7dEG3c",
-  authDomain: "notifi-457d2.firebaseapp.com",
-  projectId: "notifi-457d2",
-  storageBucket: "notifi-457d2.appspot.com",
-  messagingSenderId: "807342511177",
-  appId: "1:807342511177:web:87b8eded14b751cbec1b98",
-  measurementId: "G-7VRSQ4KPPR"
-};
-
-// Initialize Firebase
-initializeApp(firebaseConfig);
-const db = getFirestore();
-const colRef = collection(db, 'leaderboard')
 
 // queries
-const q = query(colRef, where("name", "==", "Zoe F"), orderBy('createdAt'))
+// const q = query(colRef, where("name", "==", "Zoe F"), orderBy('createdAt'))
 // realtime collection data
 // onSnapshot(q, (snapshot) => {
 //     let leaderboard = []
@@ -45,8 +27,12 @@ const q = query(colRef, where("name", "==", "Zoe F"), orderBy('createdAt'))
 
 
 const PlayerRow = ({id, rank, name})=>{
+
+    const [isSending, setSending] = useState(false)
+
     const sendText = async () =>{
 
+        setSending(true)
         console.log(id)
 
         // TODO: send POST request to your sms endpoint w/ player id
@@ -54,15 +40,9 @@ const PlayerRow = ({id, rank, name})=>{
             method:"POST",
             body: JSON.stringify({userId: id})
         })
-        //change msgSent from false to true
-        const db = getFirestore();
-        const docRef = collection(db, 'leaderboard', sendText.userId)
-        updateDoc( docRef,{
-            title: 'updated title'
-        })
-        .then(()=>{
-            updateForm.rest
-        })
+
+        setSending(false)
+        
     }
 
     
@@ -73,7 +53,14 @@ const PlayerRow = ({id, rank, name})=>{
             <h2>{rank}</h2>
             <h2>{name}</h2>
             <Spacer />
-            <Button size='sm' onClick={sendText} className={styles.text}>Send Text</Button>
+            <Button 
+                size='sm' 
+                onClick={sendText} 
+                className={styles.text}
+                disabled={isSending}
+            >
+                Send Text
+            </Button>
         </section>
         </div>
     )
--- pages/api/sms.js
+++ pages/api/sms.js
@@ -2,15 +2,49 @@
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
 import twilio from "twilio"
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, onSnapshot,
+    addDoc, deleteDoc, doc,
+    query, where,
+    setDoc,
+    orderBy, serverTimestamp,
+    updateDoc } from "firebase/firestore";
 
 
+    const firebaseConfig = {
+      apiKey: "AIzaSyA5qsxUaqCK926zTNiTSFoFHy-BK7dEG3c",
+      authDomain: "notifi-457d2.firebaseapp.com",
+      projectId: "notifi-457d2",
+      storageBucket: "notifi-457d2.appspot.com",
+      messagingSenderId: "807342511177",
+      appId: "1:807342511177:web:87b8eded14b751cbec1b98",
+      measurementId: "G-7VRSQ4KPPR"
+      };
+      
+      // Initialize Firebase
+    initializeApp(firebaseConfig);
+    const db = getFirestore();
 
 export default async function handler (req, res) {
-  try {
-    if (req.method !== "POST") {
+    if (req.method === "POST") {
+      const { userId } = JSON.parse (req.body)
+      const twilioResp = await sendTwilio(userId)
+      // todo: check on the twilioResp and decide whether to continue onto firestore stuff 
+      const firestoreResp = await updateFirestore(userId)
+      // return a thing to the client
+      res.status(200).json({status: "i did the thing!"})
+    }
+    else if (req.method ==="GET"){
+      const resp = await getFirestoreDocs()
+      // fetch firestore info (do that in a new function below)
+    }
+    else{
       return res.status(405).json({ error: "method not supported" })
     }
+}
 
+async function sendTwilio(userId){
+  try {
     const accountSid = process.env.TWILIO_ACCT_SID;
     const authToken = process.env.TWILIO_AUTH_TOKEN;
     const client = new twilio(accountSid, authToken);
@@ -18,12 +52,11 @@ export default async function handler (req, res) {
     // return console.log(player)
     // TODO: Get the player's phone number from scorebot
     // { "playerId": "12345" }
-    const { userId } = JSON.parse (req.body)
     // console.log(req.body)
-    console.log(userId)
+    // console.log(userId)
     const playerRequest = await fetch(`https://scorebot-api-service-q3nu3.ondigitalocean.app/v1/players/${userId}`)
     const playerData = await playerRequest.json()
-    console.log(playerData)
+    // console.log(playerData)
     // TODO: Send message to the player
     const message = await client.messages.create({
       body: 'winner winner chicken dinner!!',
@@ -31,9 +64,29 @@ export default async function handler (req, res) {
       mediaUrl: ['https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg'],
       to: playerData.data.phone
     })
-    return res.json({ message: "Success!" })
+    return { message: "Success!" }
   } catch (error) {
     console.error(error)
-    // TODO: return api handler error response
+      return {error: "it all went sideways!"}
+    // return something here, error state!
   }
+}
+
+async function updateFirestore(userId){
+
+    //FIREBASE UPDATEY STUFF
+    const ref = setDoc(doc(db, "leaderboard", userId), {
+      id: userId,
+      notified: new Date()
+    })
+
+      console.log("hi there i'm doin a thing!")
+      console.log(ref)
+
+    return {status: "sent! probably!"}
+}
+
+async function getFirestoreDocs(){
+  const snapshot = await getDocs(doc(db, "leaderboard"))
+  console.log(snapshot.data())
 }
